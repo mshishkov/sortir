0352a8285d19f6b5d96b7341d39dc03915ec40d4
mshishkov <dieselme@mail.ru>
2016-05-30 18:57:41 +0300

    first step to angular )

--- game.js
+++ game.js
@@ -1,18 +1,39 @@
-(function() {
-    'use strict';
+'use strict';
 
-    angular
-        .module('game', [
-            // 'ngRoute',
-            // 'game.admin',
-            // 'game.profile',
-            // 'game.settings',
-            // 'game.buy',
-            // 'game.users',
-            // 'game.start',
-            // 'game.join',
-            // 'game.game',
-            // 'game.chat',
-        ]);
+angular
+.module('gameApp', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
 
-})();
+    $urlRouterProvider.otherwise('/');
+    
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'Game/index.html'
+        })
+        .state('gameplay', {
+            url: '/',
+            templateUrl: 'Game/Play/playView.html'
+        })
+        .state('home.start', {
+            url: '/start',
+            templateUrl: 'Game/Start/startView.html', 
+            // controller: 'StartController as start'
+        })
+        .state('home.join', {
+            url: '/join',
+            templateUrl: 'Game/Join/joinView.html', 
+            // controller: 'JoinController as join'
+        })
+        .state('home.users', {
+            url: '/users',
+            templateUrl: 'Game/Users/usersView.html', 
+            // controller: 'UsersController as users'
+        })
+        .state('home.chat', {
+            url: '/chat',
+            templateUrl: 'Game/Chat/chatView.html', 
+            // controller: 'ChatController as chat'
+        })
+        ;
+});
